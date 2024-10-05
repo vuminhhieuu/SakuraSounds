72ccaa0689b681fd06f4b05be3760b157fa14000
hieuvm <vuhieu26102004@gmail.com>
2024-10-06 01:16:30 +0700

    fix conflict

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-
-const songController = require('../controllers/songController')
-
-router.use('/', songController.getHomePage)
-
-module.exports = router
